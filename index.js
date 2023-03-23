d532184e98ea30d657fc942a7a04bc138787802e
Sandy Arfian <sandyarfian.2019@gmail.com>
2023-03-23 10:23:17 +0700

    tambah

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => res.send('hello sunday'))
+app.get("/ping", (req, res) => res.send('pong'))
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
